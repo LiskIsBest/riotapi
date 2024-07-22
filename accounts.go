4dd98730a71ef55b644c60d3c89ba054dfa14395
Darien Moore <liskisbest@proton.me>
2024-07-22 15:42:41 -0500

    endpoint for account by riot id

---
+++ accounts.go
@@ -0,0 +1,46 @@
+package riotapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type AccountDto struct {
+	Puuid    string `json:"puuid"`
+	GameName string `json:"gameName"`
+	TagLine  string `json:"tagLine"`
+}
+
+type ActiveShardDto struct {
+	Puuid       string `json:"puuid"`
+	Game        string `json:"game"`
+	ActiveShard string `json:"activeShard"`
+}
+
+type AccountsService struct {
+	client *RiotApi
+}
+
+func (s AccountsService) AccountByRiotID(region string, gameName string, tagLine string) (AccountDto, error) {
+	region, err := convertPlatform2Regional(region)
+	if err != nil {
+		return AccountDto{}, err
+	}
+	hostURL := "https://" + region + baseHostURL
+	endpoint := fmt.Sprintf("%s/riot/account/v1/accounts/by-riot-id/%s/%s", hostURL, gameName, tagLine)
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return AccountDto{}, err
+	}
+	body, err := s.client.Do(req)
+	if err != nil {
+		return AccountDto{}, err
+	}
+	var account AccountDto
+	err = json.Unmarshal(body, &account)
+	if err != nil {
+		return AccountDto{}, err
+	}
+	return account, nil
+}
