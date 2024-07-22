7d0c1646e0545e0a3abb25c25165b87778336d39
Darien Moore <liskisbest@proton.me>
2024-07-22 16:58:14 -0500

    added statusOK check

--- riotapi.go
+++ riotapi.go
@@ -1,6 +1,7 @@
 package riotapi
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -39,6 +40,9 @@ func (r RiotApi) Do(req *http.Request) ([]byte, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK{
+		return nil, fmt.Errorf("request failed: %s", resp.Status)
+	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
