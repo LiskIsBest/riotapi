14c9012ff577a192cd2e216621a065334630a03b
Darien Moore <liskisbest@proton.me>
2024-07-22 16:57:58 -0500

    added AccountByPuuid method

--- accounts.go
+++ accounts.go
@@ -22,13 +22,32 @@ type AccountsService struct {
 	client *RiotApi
 }
 
+func (s AccountsService) AccountByPuuid(puuid string) (AccountDto, error) {
+	endpoint := fmt.Sprintf("%s/riot/account/v1/accounts/by-puuid/%s", baseHostURL2, puuid)
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		return AccountDto{}, err
+	}
+	req.Header.Add("X-Riot-Token", s.client.key)
+	body, err := s.client.Do(req)
+	if err != nil {
+		return AccountDto{}, err
+	}
+	var account AccountDto
+	err = json.Unmarshal(body, &account)
+	if err != nil {
+		return AccountDto{}, err
+	}
+	return account, nil
+}
+
 func (s AccountsService) AccountByRiotID(gameName string, tagLine string) (AccountDto, error) {
 	endpoint := fmt.Sprintf("%s/riot/account/v1/accounts/by-riot-id/%s/%s", baseHostURL2, gameName, tagLine)
 	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
 		return AccountDto{}, err
 	}
-	req.Header.Add("X-Riot-Token",s.client.key)
+	req.Header.Add("X-Riot-Token", s.client.key)
 	body, err := s.client.Do(req)
 	if err != nil {
 		return AccountDto{}, err
