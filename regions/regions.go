b65cc3d355c4f23b6f046be84c85659431e00cea
Darien Moore <liskisbest@proton.me>
2024-07-22 14:26:03 -0500

    dropped the host portion of the string representation

--- regions/regions.go
+++ regions/regions.go
@@ -3,29 +3,29 @@ package regions
 // platorm routing values
 // https://developer.riotgames.com/docs/lol#routing-values_platform-routing-values
 const (
-	BR1 = "br1.api.riotgames.com"
-	EUN = "eun1.api.riotgames.com"
-	EUW = "euw1.api.riotgames.com"
-	JP1 = "jp1.api.riotgames.com"
-	KR  = "kr.api.riotgames.com"
-	LA1 = "la1.api.riotgames.com"
-	LA2 = "la2.api.riotgames.com"
-	NA1 = "na1.api.riotgames.com"
-	OC1 = "oc1.api.riotgames.com"
-	TR1 = "tr1.api.riotgames.com"
-	RU  = "ru.api.riotgames.com"
-	PH2 = "ph2.api.riotgames.com"
-	SG2 = "sg2.api.riotgames.com"
-	TH2 = "th2.api.riotgames.com"
-	TW2 = "tw2.api.riotgames.com"
-	VN2 = "vn2.api.riotgames.com"
+	BR1 = "br1"
+	EUN = "eun1"
+	EUW = "euw1"
+	JP1 = "jp1"
+	KR  = "kr"
+	LA1 = "la1"
+	LA2 = "la2"
+	NA1 = "na1"
+	OC1 = "oc1"
+	TR1 = "tr1"
+	RU  = "ru"
+	PH2 = "ph2"
+	SG2 = "sg2"
+	TH2 = "th2"
+	TW2 = "tw2"
+	VN2 = "vn2"
 )
 
 // region routing values
 // https://developer.riotgames.com/docs/lol#routing-values_regional-routing-values
 const (
-	AMERICAS = "americas.api.riotgames.com"
-	ASIA     = "asia.api.riotgames.com"
-	EUROPE   = "europe.api.riotgames.com"
-	SEA      = "sea.api.riotgames.com"
+	AMERICAS = "americas"
+	ASIA     = "asia"
+	EUROPE   = "europe"
+	SEA      = "sea"
 )
