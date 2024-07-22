2b0181bd9b9c92fd4030d4147477ab668198963b
Darien Moore <liskisbest@proton.me>
2024-07-22 11:13:46 -0500

    added links for riot games api documentation

--- riotapi.go
+++ riotapi.go
@@ -1,6 +1,9 @@
 package riotapi
 
-type Test struct {
-	Fname string
-	lname string
-}
+/*
+link for getting a key: https://developer.riotgames.com/
+enpoints: https://developer.riotgames.com/apis
+riot docs:
+	- https://developer.riotgames.com/docs/portal#_getting-started
+	- https://developer.riotgames.com/docs/lol
+*/
